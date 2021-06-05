02acc24f4040433f6fb6cb82b5755b22681caa8d
astlok <64102363+astlok@users.noreply.github.com>
2021-06-05 21:48:37 +0300

    update idx

--- schema.sql
+++ schema.sql
@@ -131,7 +131,7 @@ create index pgb_first_idx on dbforum.post (thread_id, parent);
 create index pgb_sec_idx on dbforum.post ((tree[1]), id);
 create index pgb_third_idx on dbforum.post ((tree[1]) DESC, tree, id);
 create index pgb_fourth_idx on dbforum.post (tree, id);
-create index pgb_fifth_idx on dbforum.post using gin (tree);
+-- create index pgb_fifth_idx on dbforum.post using gin (tree);
 --TODO:
 -- create index pgb_test_idx on dbforum.post (thread_id, id);
 
